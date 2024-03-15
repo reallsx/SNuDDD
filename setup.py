e6f2aed9cdfe203ce62c1a3f40e14de214514841
Shixiao Liang <liangsx@rice.edu>
2024-03-15 10:30:28 -0500

    initial edit

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='snudd',
-    url='https://github.com/dwpamaral/SNuDD.git',
+    name='snuddd',
+    url='https://github.com/reallsx/SNuDDD.git',
     version="1.0",
-    author='Dorian Amaral',
-    packages=['snudd'],
+    packages=['snuddd'],
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
